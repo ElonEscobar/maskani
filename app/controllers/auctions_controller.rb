0abb4409cba1ebe207e73dc59c96109c9d8576a7
Dan Muendo <danwalker254254@gmaail.com>
2023-02-17 12:16:10 +0300

    updated auction controller

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,2 +1,44 @@
 class AuctionsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+    def index
+        auctions = Auction.all
+        render json: auctions, status: :ok
+    end
+
+    def show
+        auction = Auction.find(params[:id])
+        render json: auction, status: :ok
+    end
+
+    def create
+        auction = Auction.create!(auction_params)
+        render json: auction, status: :created
+    end
+
+    def update
+        auction = Auction.update!(auction_params)
+        render json: auction, status: :accepted
+    end
+
+    def destroy
+        auction = Auction.find(params[:id])
+        auction.destroy!
+        head :no_content
+    end
+
+    private
+
+    def auction_params
+        params.permit(:name, :price, :description, :end_date)
+    end
+
+    def render_not_found_response
+        render json: { error: "Auction not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
--- app/models/auction.rb
+++ app/models/auction.rb
@@ -2,6 +2,7 @@ class Auction < ApplicationRecord
     after_initialize :init
 
     def init
+        self.valid ||= true
         self.is_verified ||= false
     end
 end
